c4f633a0358f7deda4a67cbbab2c442f965b2bc1
SamuraiT <t.yasukawa01@gmail.com>
2014-07-18 21:53:15 +0900

    add home dir

--- vim.py
+++ vim.py
@@ -7,11 +7,13 @@ Distributed under the GNU General Public License at
 gnu.org/licenses/gpl.html.
 """
 
+import os
+home = os.path.expanduser("~")
 
 def main():
     with open('vimrc') as f:
         settings = f.read()
-    with open('~/.vimrc', 'w+') as vimrc:
+    with open(os.path.join(home, '.vimrc'), 'w+') as vimrc:
         vimrc.write(settings)
         print('done')
 
